9d35ef56d661b1414b743a13c27cdab8e98cceea
Atick Faisal <atickfaisal@gmail.com>
2025-12-06 10:11:21 +0300

    🎉 feat: initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2025 Atick Faisal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.io.FileInputStream
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Properties
+
+val keystorePropertiesFile: File = rootProject.file("keystore.properties")
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    // ... Application Version ...
+    val majorUpdateVersion = 1
+    val minorUpdateVersion = 2
+    val patchVersion = 3
+
+    val mVersionCode = majorUpdateVersion.times(10_000)
+        .plus(minorUpdateVersion.times(100))
+        .plus(patchVersion)
+
+    val mVersionName = "$majorUpdateVersion.$minorUpdateVersion.$patchVersion"
+    val formatter = DateTimeFormatter.ofPattern("yyyy_MM_dd_hh_mm_a")
+    val currentTime = LocalDateTime.now().format(formatter)
+
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 36
+        versionCode = mVersionCode
+        versionName = mVersionName
+        applicationId = "dev.atick.shorts"
+    }
+
+    signingConfigs {
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                val keystoreProperties = Properties()
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        release {
+            isMinifyEnabled = true
+            applicationVariants.all {
+                outputs.all {
+                    (this as BaseVariantOutputImpl).outputFileName =
+                        rootProject.name.replace(" ", "_") + "_" +
+                                (buildType.name + "_v") +
+                                (versionName + "_") +
+                                "${currentTime}.apk"
+                    println(outputFileName)
+                }
+            }
+            signingConfig = if (keystorePropertiesFile.exists()) {
+                signingConfigs.getByName("release")
+            } else {
+                println(
+                    "keystore.properties file not found. Using debug key. Read more here: " +
+                            "https://atick.dev/Jetpack-Android-Starter/github",
+                )
+                signingConfigs.getByName("debug")
+
+            }
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    namespace = "dev.atick.shorts"
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+}
